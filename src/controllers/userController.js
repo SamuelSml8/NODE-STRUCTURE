fcc9125f760311c92e258a96ae6106ef5182a4a9
Samuel Sml <veramirandasamuel6@gmail.com>
2024-03-15 07:54:06 -0500

    create endpoints get user by name and get user by id, routes and controllers

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -19,6 +19,62 @@ const userController = {
     }
   },
 
+  getUserById: async (req, res) => {
+    try {
+      const userId = req.params.id;
+      const userFound = await User.findById(userId);
+
+      if (!userFound) {
+        return res.status(404).json({
+          ok: false,
+          message: "User not found",
+          data: null,
+        });
+      }
+
+      return res.status(200).json({
+        ok: true,
+        message: "User found",
+        data: userFound,
+      });
+    } catch (error) {
+      console.error("Error getting user by id: ", error);
+      res.status(500).json({
+        ok: false,
+        message: "Internal Server error",
+        data: null,
+      });
+    }
+  },
+
+  getUserByName: async (req, res) => {
+    try {
+      const userName = req.params.name;
+      const userFound = await User.findOne({name: userName});
+
+      if (!userFound) {
+        return res.status(404).json({
+          ok: false,
+          message: "User not found",
+          data: null,
+        });
+      }
+
+      return res.status(200).json({
+        ok: true,
+        message: "User found",
+        data: userFound,
+      });
+    } catch (error) {
+      console.log("Error getting user by name: ", error);
+      res.status(500).json({
+        ok: false,
+        message: "Internal Server error",
+        data: null,
+      });
+    }
+  },
+
   createUser: async (req, res) => {
     const userData = req.body;
     try {
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,10 @@ const userController = require("../controllers/userController.js");
 
 router.get("/api/v1/users", userController.getAllUsers);
 
+router.get("/api/v1/users/id/:id", userController.getUserById);
+
+router.get("/api/v1/users/name/:name", userController.getUserByName);
+
 router.post("/api/v1/users", userController.createUser);
 
 module.exports = router;
